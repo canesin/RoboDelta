f71353f9f4971a61624d664d0fe66ebb6a9f5f06
Fábio César Canesin <fc@canesin.com>
2011-10-28 13:31:07 -0200

    -

--- delta.C
+++ delta.C
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <math.h>
  
-// Constantes geométricas do rôbo
+ // Constantes geométricas do rôbo
  // (Figuras no relatório)
  const float e = 30.0; // Lado do triangulo que circunscreve o manipulador
  const float f = 112.5; // Lado da do triangulo que circunscreve a base
@@ -17,6 +17,7 @@
  const float sin30 = 0.5;
  const float tan30 = 1/sqrt3;
  
+ // -------------------------------- Cinemática direta -----------------------------------------
  // Cinemática direta: (theta1, theta2, theta3) -> (x0, y0, z0)
  // Status do retorno: 0=OK, -1=posição fora do espaço de trabalho
  int delta_calcForward(float theta1, float theta2, float theta3, float &x0, float &y0, float &z0) {
@@ -66,33 +67,35 @@
      y0 = (a2*z0 + b2)/dnm; // Devolve a coordenada Y do ponto do manipulador
      return 0;
  }
- 
- // inverse kinematics
- // helper functions, calculates angle theta1 (for YZ-pane)
+ // --------------------------------Fim Cinemática direta ---------------------------------------
+
+ // -------------------------------- Cinemática inversa -----------------------------------------
+ // Funções de apoio, calcula anglo theta1 (para o plano YZ)
  int delta_calcAngleYZ(float x0, float y0, float z0, float &theta) {
      float y1 = -0.5 * 0.57735 * f; // f/2 * tg 30
-     y0 -= 0.5 * 0.57735    * e;    // shift center to edge
+     y0 -= 0.5 * 0.57735    * e;    // realiza projeção da junta
      // z = a + b*y
      float a = (x0*x0 + y0*y0 + z0*z0 +rf*rf - re*re - y1*y1)/(2*z0);
      float b = (y1-y0)/z0;
      // discriminant
      float d = -(a+b*y1)*(a+b*y1)+rf*(b*b*rf+rf); 
-     if (d < 0) return -1; // non-existing point
-     float yj = (y1 - a*b - sqrt(d))/(b*b + 1); // choosing outer point
+     if (d < 0) return -1; // Ponto fora do espaço de trabalho
+     float yj = (y1 - a*b - sqrt(d))/(b*b + 1); // Escolhendo ponto externo
      float zj = a + b*yj;
      theta = 180.0*atan(-zj/(y1 - yj))/pi + ((yj>y1)?180.0:0.0);
      return 0;
  }
  
- // inverse kinematics: (x0, y0, z0) -> (theta1, theta2, theta3)
- // returned status: 0=OK, -1=non-existing position
+ // Cinemática inversa: (x0, y0, z0) -> (theta1, theta2, theta3)
+ // Status do retorno: 0=OK, -1=posição fora do espaço de trabalho
  int delta_calcInverse(float x0, float y0, float z0, float &theta1, float &theta2, float &theta3) {
      theta1 = theta2 = theta3 = 0;
      int status = delta_calcAngleYZ(x0, y0, z0, theta1);
-     if (status == 0) status = delta_calcAngleYZ(x0*cos120 + y0*sin120, y0*cos120-x0*sin120, z0, theta2);  // rotate coords to +120 deg
-     if (status == 0) status = delta_calcAngleYZ(x0*cos120 - y0*sin120, y0*cos120+x0*sin120, z0, theta3);  // rotate coords to -120 deg
+     if (status == 0) status = delta_calcAngleYZ(x0*cos120 + y0*sin120, y0*cos120-x0*sin120, z0, theta2);  // rotaciona +120 deg
+     if (status == 0) status = delta_calcAngleYZ(x0*cos120 - y0*sin120, y0*cos120+x0*sin120, z0, theta3);  // rotaciona -120 deg
      return status;
  }
+ // -------------------------------- Fim Cinemática inversa ---------------------------------------
 
 using namespace std;
 int main(){
