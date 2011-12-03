bbecafded59edf71455410e9f4493bb151b94056
Fábio César Canesin <fc@canesin.com>
2011-12-03 15:54:38 -0200

    initial work

---
+++ servo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import serial
+
+usbport = '/dev/tty.usbserial-FTALLOK2'
+ser = serial.Serial(usbport, 9600, timeout=1)
+
+def move(servo, angle):
+	
+	if (0 <= angle <= 180):
+		ser.write(chr(255))
+		ser.write(chr(servo))
+		ser.write(chr(angle))
+	else:
+		print "Servo angle must be an integer between 0 and 180.\n"
